4be8f00ad39e5761841988725e060b7d44f7ab7d
hiverkiya <hiverkiya@gmail.com>
2020-05-03 02:59:26 +0530

    Fixed AdminBusinessService.java

--- stackoverflow-service/src/main/java/com/upgrad/stackoverflow/service/business/AdminBusinessService.java
+++ stackoverflow-service/src/main/java/com/upgrad/stackoverflow/service/business/AdminBusinessService.java
@@ -1,80 +1,88 @@
 package com.upgrad.stackoverflow.service.business;
 
-import com.upgrad.stackoverflow.service.dao.AdminDao;
+import com.upgrad.stackoverflow.service.common.JwtTokenProvider;
 import com.upgrad.stackoverflow.service.dao.UserDao;
 import com.upgrad.stackoverflow.service.entity.UserAuthEntity;
 import com.upgrad.stackoverflow.service.entity.UserEntity;
-import com.upgrad.stackoverflow.service.exception.AuthorizationFailedException;
-import com.upgrad.stackoverflow.service.exception.UserNotFoundException;
+import com.upgrad.stackoverflow.service.exception.AuthenticationFailedException;
+import com.upgrad.stackoverflow.service.exception.SignOutRestrictedException;
+import com.upgrad.stackoverflow.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
 
 @Service
-public class AdminBusinessService {
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
-package com.upgrad.stackoverflow.service.business;
-
-import com.upgrad.stackoverflow.service.dao.AdminDao;
-import com.upgrad.stackoverflow.service.dao.UserDao;
-import com.upgrad.stackoverflow.service.entity.UserAuthEntity;
-import com.upgrad.stackoverflow.service.entity.UserEntity;
-import com.upgrad.stackoverflow.service.exception.AuthorizationFailedException;
-import com.upgrad.stackoverflow.service.exception.UserNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-    @Service
-    public class AdminBusinessService {
-        @Autowired
-        private UserDao userDao;
-        @Autowired
-        private AdminDao adminDao;
-
-        public AdminBusinessService() {
-        }
-
-        @Transactional(
-                propagation = Propagation.REQUIRED
-        )
-        public UserEntity deleteUser(String authorization, String uuid) throws AuthorizationFailedException, UserNotFoundException {
-            UserAuthEntity userAuthEntity = this.userDao.getUserAuthByAccesstoken(authorization);
-            if (userAuthEntity == null) {
-                throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
-            } else if (userAuthEntity.getLogoutAt() != null) {
-                throw new AuthorizationFailedException("ATHR-002", "User is signed out");
-            } else if (userAuthEntity.getUser().getRole().equals("admin")) {
-                UserEntity userEntity = this.adminDao.getUserByUuid(uuid);
-                if (userEntity == null) {
-                    throw new UserNotFoundException("USR-001", "User with entered uuid to be deleted does not exist");
-                } else {
-                    return this.adminDao.deleteUser(userEntity);
-                }
-            } else {
-                throw new AuthorizationFailedException("ATHR-003", "Unauthorized Access, Entered user is not an admin");
-            }
-        }
-    }
+public class UserBusinessService {
 
     @Autowired
     private UserDao userDao;
 
     @Autowired
-    private AdminDao adminDao;
+    private PasswordCryptographyProvider passwordCryptographyProvider;
 
     /**
-     * The method implements the business logic for userDelete endpoint.
+     * The method implements the business logic for signup endpoint.
      */
     @Transactional(propagation = Propagation.REQUIRED)
-    public UserEntity deleteUser(String authorization, String uuid) throws AuthorizationFailedException, UserNotFoundException {
-        UserAuthEntity userAuthEntity = userDao.getUserAuthByAccesstoken(authorization);
+    public UserEntity signup(UserEntity userEntity) throws SignUpRestrictedException {
+        String[] encryptedText = passwordCryptographyProvider.encrypt(userEntity.getPassword());
+        userEntity.setSalt(encryptedText[0]);
+        userEntity.setPassword(encryptedText[1]);
+        if(userDao.getUserByUsername(userEntity.getUserName())!=null ||userDao.getUserByEmail(userEntity.getEmail())!=null){
+            throw new SignUpRestrictedException("409", "User Already Exist");
+        }
+        return userDao.createUser(userEntity);
+    }
 
+    /**
+     * The method implements the business logic for signin endpoint.
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthEntity authenticate(String username, String password) throws AuthenticationFailedException {
+        UserEntity userEntity = userDao.getUserByUsername(username);
+        if(userEntity==null){
+            throw new AuthenticationFailedException("401","User with email not found");
+        }
+        final String encryptedPassword = passwordCryptographyProvider.encrypt(password, userEntity.getSalt());
+        if(encryptedPassword.equals(userEntity.getPassword())){
+            JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(encryptedPassword);
+            UserAuthEntity userAuthToken = new UserAuthEntity();
+            userAuthToken.setUser(userEntity);
+            final ZonedDateTime now = ZonedDateTime.now();
+            final ZonedDateTime expiresAt = now.plusHours(8);
+            userAuthToken.setAccessToken(jwtTokenProvider.generateToken(userEntity.getUuid(), now, expiresAt));
+            userAuthToken.setLoginAt(now);
+            userAuthToken.setExpiresAt(expiresAt);
+            userAuthToken.setUuid(userEntity.getUuid());
+
+            userDao.createUserAuth(userAuthToken);
+//            userDao.updateUser(userEntity);
+
+            return userAuthToken;
+        }
+        else{
+            throw new AuthenticationFailedException("ATH-002", "Password Failed");
+        }
+    }
+
+    /**
+     * The method implements the business logic for signout endpoint.
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthEntity signout(String authorization) throws SignOutRestrictedException {
+        if(authorization==null)
+            throw new SignOutRestrictedException("400", "Access Token is null");
+        UserAuthEntity userAuthEntity = userDao.getUserAuthByAccesstoken(authorization);
+        if(userAuthEntity==null)
+            throw new SignOutRestrictedException("400", "Invalid Access Token");
+        if(userAuthEntity.getLogoutAt()!=null)
+            throw new SignOutRestrictedException("404","Already Logged Out");
+
+        final ZonedDateTime now = ZonedDateTime.now();
+        userAuthEntity.setLogoutAt(now);
+        return userDao.updateUserAuth(userAuthEntity);
     }
 }
